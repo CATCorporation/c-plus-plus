99ce19ea2caa747e38f93323559b3ba197204715
Stitchou <la_muerte_97216@hotmail.com>
2015-05-26 18:55:27 +0200

    dossier pour le setup

--- build-Client-Desktop_Qt_5_4_1_MinGW_32bit-Release/release/moc_client.cpp
+++ build-Client-Desktop_Qt_5_4_1_MinGW_32bit-Release/release/moc_client.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_Client_t {
-    QByteArrayData data[9];
-    char stringdata[94];
+    QByteArrayData data[10];
+    char stringdata[110];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,12 +37,13 @@ QT_MOC_LITERAL(4, 38, 7), // "timeout"
 QT_MOC_LITERAL(5, 46, 13), // "receivePseudo"
 QT_MOC_LITERAL(6, 60, 3), // "psd"
 QT_MOC_LITERAL(7, 64, 14), // "sendDisconnect"
-QT_MOC_LITERAL(8, 79, 14) // "againConnected"
+QT_MOC_LITERAL(8, 79, 14), // "againConnected"
+QT_MOC_LITERAL(9, 94, 15) // "on_aide_clicked"
 
     },
     "Client\0on_pushButton_clicked\0\0readSoc\0"
     "timeout\0receivePseudo\0psd\0sendDisconnect\0"
-    "againConnected"
+    "againConnected\0on_aide_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -52,7 +53,7 @@ static const uint qt_meta_data_Client[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -60,18 +61,20 @@ static const uint qt_meta_data_Client[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   44,    2, 0x08 /* Private */,
-       3,    0,   45,    2, 0x08 /* Private */,
-       4,    0,   46,    2, 0x08 /* Private */,
-       5,    1,   47,    2, 0x08 /* Private */,
-       7,    0,   50,    2, 0x08 /* Private */,
-       8,    0,   51,    2, 0x08 /* Private */,
+       1,    0,   49,    2, 0x08 /* Private */,
+       3,    0,   50,    2, 0x08 /* Private */,
+       4,    0,   51,    2, 0x08 /* Private */,
+       5,    1,   52,    2, 0x08 /* Private */,
+       7,    0,   55,    2, 0x08 /* Private */,
+       8,    0,   56,    2, 0x08 /* Private */,
+       9,    0,   57,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, QMetaType::QString,    6,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -89,6 +92,7 @@ void Client::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
         case 3: _t->receivePseudo((*reinterpret_cast< QString(*)>(_a[1]))); break;
         case 4: _t->sendDisconnect(); break;
         case 5: _t->againConnected(); break;
+        case 6: _t->on_aide_clicked(); break;
         default: ;
         }
     }
@@ -119,13 +123,13 @@ int Client::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 7)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
+        _id -= 7;
     }
     return _id;
 }
